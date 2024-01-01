527063e7e7e59add4259fb2c6a2ea49a002958f7
andrey yudin <onetapandrey@gmail.com>
2024-01-01 17:37:14 +0300

    add brain-calc.js

---
+++ bin/brain-calc.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+import readlineSync from "readline-sync";
+
+console.log("Welcome to the Brain Games!");
+const userName = readlineSync.question("May I have your name? ");
+console.log(`Hello, ${userName}`);
+console.log("What is the result of the expression?");
+
+const operationsArray = ["+", "-", "*"];
+const minimumNumber = 1;
+const maximumNumber = 100;
+let randomFirstOperand = 0;
+let randomSecondOperand = 0;
+let randomOperator = "";
+let mathematicalQuestion = 0;
+let mathematicalQuestionToString = "";
+let testIsPassed = true;
+
+for (let i = 0; i < 3; i += 1) {
+  randomFirstOperand = Math.floor(
+    Math.random() * (maximumNumber - minimumNumber) + minimumNumber
+  );
+  randomSecondOperand = Math.floor(
+    Math.random() * (maximumNumber - minimumNumber) + minimumNumber
+  );
+  randomOperator = operationsArray[Math.floor(Math.random() * (2 - 0) + 0)];
+  if (randomOperator === "+") {
+    mathematicalQuestion = randomFirstOperand + randomSecondOperand;
+  } else if (randomOperator === "-") {
+    mathematicalQuestion = randomFirstOperand - randomSecondOperand;
+  } else if (randomOperator === "*") {
+    mathematicalQuestion = randomFirstOperand * randomSecondOperand;
+  }
+  mathematicalQuestionToString = `${randomFirstOperand.toString()} ${randomOperator} ${randomSecondOperand.toString()}`;
+  console.log(`Question: ${mathematicalQuestionToString}`);
+  const answerToQuestion = readlineSync.question("Your answer: ");
+  if (answerToQuestion === mathematicalQuestion.toString()) {
+    console.log("Correct!");
+  } else {
+    console.log(
+      `'${answerToQuestion}' is wrong answer. Correct answer was '${mathematicalQuestion}'.`
+    );
+    console.log(`Let's try again, ${userName}!`);
+    testIsPassed = false;
+    break;
+  }
+}
+
+if (testIsPassed === true) {
+  console.log(`Congratulations, ${userName}`);
+}
