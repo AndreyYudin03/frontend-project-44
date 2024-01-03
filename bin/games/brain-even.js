9452e362715242105715dc3619bf2754a34dfded
andrey yudin <onetapandrey@gmail.com>
2024-01-03 17:39:05 +0300

    refactoring brain-even.js

--- bin/games/brain-even.js
+++ bin/games/brain-even.js
@@ -1,46 +1,28 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
+import { welcomeUser, runGame } from "../../src/index.js";
 
-console.log("Welcome to the Brain Games!");
-const userName = readlineSync.question("May I have your name? ");
-console.log(`Hello, ${userName}`);
+const userName = welcomeUser();
 
 console.log("Answer \"yes\" if the number is even, otherwise answer \"no\".");
 
-const correctAnswers = ["yes", "no"];
-const minimumNumber = 1;
-const maximumNumber = 100;
-let testIsPassed = true;
-
-for (let i = 0; i < 3; i += 1) {
+const generateEvenQuestion = () => {
+  const minimumNumber = 1;
+  const maximumNumber = 100;
   const number = Math.floor(
     Math.random() * (maximumNumber - minimumNumber) + minimumNumber
   );
-  console.log(`Question: ${number}`);
-  const answerToQuestion = readlineSync.question("Your answer: ");
-  if (number % 2 === 0 && answerToQuestion === "no") {
-    console.log(
-      `'no' is wrong answer. Correct answer was 'yes'.\nLet's try again, ${userName}`
-    );
-    testIsPassed = false;
-    break;
-  } else if (number % 2 !== 0 && answerToQuestion === "yes") {
-    console.log(
-      `'yes' is wrong answer. Correct answer was 'no'.\nLet's try again, ${userName}`
-    );
-    testIsPassed = false;
-    break;
-  } else if (correctAnswers.includes(answerToQuestion) === false) {
-    console.log(
-      `'${answerToQuestion}' is wrong answer.\nLet's try again, ${userName}`
-    );
-    testIsPassed = false;
-    break;
-  } else {
-    console.log("Correct!");
-  }
-}
+  return number;
+};
 
-if (testIsPassed === true) {
-  console.log(`Congratulations, ${userName}`);
-}
+const correctAnswerOnEvenQuestion = () => {
+  const EvenQuestion = generateEvenQuestion();
+  let correctAnswer = "";
+  if (EvenQuestion % 2 === 0) {
+    correctAnswer = "yes";
+  } else {
+    correctAnswer = "no";
+  }
+  return [EvenQuestion, correctAnswer];
+};
+
+runGame(correctAnswerOnEvenQuestion, userName);
