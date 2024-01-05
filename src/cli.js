f2e1c0c70223e47ee2999fdc7269b49d20759965
andrey yudin <onetapandrey@gmail.com>
2024-01-05 14:10:14 +0300

    refactoring cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from "readline-sync";
 
-const userNameFunction = () => {
+function welcomeUser() {
   console.log("node bin/brain-games.js\nWelcome to the Brain Games!");
   const userName = readlineSync.question("May I have your name? ");
   return console.log(`Hello, ${userName}`);
-};
+}
 
-export default userNameFunction;
+export default welcomeUser;
