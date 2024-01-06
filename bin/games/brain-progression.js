d3636ea92d9ec460514ef58a2e462c24fd65cd41
andrey yudin <onetapandrey@gmail.com>
2024-01-06 14:54:50 +0300

    refactoring

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -1,30 +1,29 @@
 #!/usr/bin/env node
 
-import { welcomeUser, runGame, getRandomNumber } from "../../src/index.js";
+import { welcomeUser, runGame, getRandomNumber } from '../../src/index.js';
 
 const userName = welcomeUser();
 
-console.log("What number is missing in the progression?");
+console.log('What number is missing in the progression?');
 
 function generateProgressionQuestion() {
   const randomFirstNumber = getRandomNumber();
 
   const [minimumProgressionLength, maximumProgressionLength] = [4, 10];
   const randomProgressionLength = Math.floor(
-    Math.random() * (maximumProgressionLength - minimumProgressionLength) +
-      minimumProgressionLength
+    Math.random() * (maximumProgressionLength - minimumProgressionLength)
+      + minimumProgressionLength,
   );
 
   const progressionArray = [];
   progressionArray.push(randomFirstNumber);
   const randomStepProgressionNumber = getRandomNumber();
-  let lastElementProgressionArray =
-    progressionArray[progressionArray.length - 1];
+  let lastElementProgressionArray = progressionArray[progressionArray.length - 1];
 
   for (let i = 0; i < randomProgressionLength; i += 1) {
     lastElementProgressionArray = progressionArray[progressionArray.length - 1];
     progressionArray.push(
-      lastElementProgressionArray + randomStepProgressionNumber
+      lastElementProgressionArray + randomStepProgressionNumber,
     );
   }
 
@@ -34,14 +33,14 @@ function generateProgressionQuestion() {
 function generateProgressionQuestionWithCorrectAnswer() {
   const progressionArray = generateProgressionQuestion();
   const randomProgressionArrayIndex = Math.floor(
-    Math.random() * (progressionArray.length - 0) + 0
+    Math.random() * (progressionArray.length - 0) + 0,
   );
 
   const hiddenNumber = progressionArray[randomProgressionArrayIndex];
 
-  progressionArray[randomProgressionArrayIndex] = "..";
+  progressionArray[randomProgressionArrayIndex] = '..';
   return [
-    progressionArray.toString().replace(/,/g, " "),
+    progressionArray.toString().replace(/,/g, ' '),
     hiddenNumber.toString(),
   ];
 }
